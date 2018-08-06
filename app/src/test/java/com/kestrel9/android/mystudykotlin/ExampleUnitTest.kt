92150f8faeb3e31daf3dea82b485ec86c1d9df69
deadlykitten-dev <deadlykitten.dev@gmail.com>
2018-08-06 21:40:06 +0900

    add gradle retrofit

---
+++ app/src/test/java/com/kestrel9/android/mystudykotlin/ExampleUnitTest.kt
@@ -0,0 +1,32 @@
+package com.kestrel9.android.mystudykotlin
+
+import org.junit.Test
+import java.util.Scanner
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+
+    }
+//    @Test
+//    fun printAdults(people : List<Person>){
+//
+//        people.filter { person -> person.adult }
+//                .forEach { println("Name = ${it.name}") }
+//
+//        val numbers = 0..10
+//
+//        numbers.mapIndexed { idx, number -> idx * number }
+//                .forEach { print("$it ") }
+//
+//    }
+}
---
+++ app/src/test/java/com/kestrel9/android/mystudykotlin/Person.kt
@@ -0,0 +1,12 @@
+package com.kestrel9.android.mystudykotlin
+
+/**
+ * MyStudyKotlin
+ * Class: Person
+ * Created by kestr on 2018-07-21.
+ *
+ * Description:
+ */
+class Person (val name : String, val age : Int) {
+    val adult = age > 19
+}
